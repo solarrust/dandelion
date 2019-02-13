47f04f22b4345f99e88d5aeb9f11f1a28d6170ba
Alena Batitskaya <solar.rust@gmail.com>
2019-02-13 17:01:30 +0700

    Tablet adaptation

--- src/media/js/modules/Header.js
+++ src/media/js/modules/Header.js
@@ -1,18 +1,17 @@
 var dom = require('../utils/DOM');
+var env = require('../utils/ENV');
 
 var $target = $('[data-transform-on-scroll]');
-var top = $target.offset().top;
 var $el = $('[data-text]:first'),
 	text = $.trim($el.text()),
 	words = text.split(' '),
-	html = '',
-	htmlSpan = '';
+	html = '';
 var height = $el.height();
 $el.parent().css({ height: height });
 
 function HeaderHelper() {
 	dom.$window.on('scroll', () => {
-		if (dom.$window.scrollTop() > top) {
+		if (dom.$window.scrollTop() > 40) {
 			$target.addClass('_scrolled');
 		} else {
 			$target.removeClass('_scrolled');
@@ -20,24 +19,25 @@ function HeaderHelper() {
 	});
 
 	dom.$window.on('load', function() {
-		for (var i = 0; i < words.length; i++) {
-			console.log(words[i]);
-			if (i == 0 || i == 1) {
-				html += "<span class='_light'>" + words[i] + (i + 1 === words.length ? '' : ' ') + '</span>';
-			} else {
-				html += '<span>' + words[i] + (i + 1 === words.length ? '' : ' ') + '</span>';
+		if (!env.isMobile) {
+			for (var i = 0; i < words.length; i++) {
+				if (i == 0 || i == 1) {
+					html += "<span class='_light'>" + words[i] + (i + 1 === words.length ? '' : ' ') + '</span>';
+				} else {
+					html += '<span>' + words[i] + (i + 1 === words.length ? '' : ' ') + '</span>';
+				}
 			}
+			$el.css({ opacity: 1 });
+			$el
+				.html(html)
+				.children()
+				.hide()
+				.each(function(i) {
+					$(this)
+						.delay(i * 300)
+						.fadeIn(words[i].length > 3 ? words[i].length * 100 : 500);
+				});
 		}
-		$el.css({ opacity: 1 });
-		$el
-			.html(html)
-			.children()
-			.hide()
-			.each(function(i) {
-				$(this)
-					.delay(i * 300)
-					.fadeIn(words[i].length > 3 ? words[i].length * 100 : 500);
-			});
 	});
 }
 
